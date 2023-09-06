33eb708b57b3a14f98554c587e4cce645c30cbdf
yannelly25 <yannellym@gmail.com>
2023-09-06 12:35:57 -0400

    added error page for favorites

--- frontend/src/components/FavoritesPage.js
+++ frontend/src/components/FavoritesPage.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/FavoritesPage.css';
 import PetCard from './PetCard';
 
 function FavoritesPage({ favoritePets, removeFromFavorites }) {
   return (
-    <div>
-      <h2>Favorite Pets</h2>
-      <div className="pet-card-container">
-        {favoritePets.map((pet) => (
-          <PetCard
-            key={pet.id}
-            pet={pet}
-            // callback function to remove pet from favorites
-            removeFromFavorites={removeFromFavorites} // Pass the correct prop name
-            isFavorite={true} // Pass isFavorite as true for all pets on FavoritesPage
-          />
-        ))}
+    <div className="pet-card-container">
+      <div className="title-container">
+        <h1>Favorite Pets 🐾 </h1>
       </div>
+      {favoritePets.length === 0 ? (
+        <div className="message-container">
+          <img src="/images/sadlab.jpg" alt="Sad Lab" className="sad-lab-image" />
+          <div className="message">
+            <h2>Uh oh, no pets have been favorited yet.</h2>
+            <p>
+              Click <Link to="/find-a-pet" className="link">HERE</Link> to find your next companion 🦴🧶.
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="pet-card-list">
+          {favoritePets.map((pet) => (
+            <PetCard key={pet.id} pet={pet} removeFromFavorites={removeFromFavorites} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
