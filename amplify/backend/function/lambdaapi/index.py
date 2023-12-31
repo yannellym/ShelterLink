6dca9c6c96f62e403d444ab2cbb4a719c62f3948
Yannelly M <yannellym@Yannellys-Air.attlocal.net>
2023-12-30 21:13:20 -0500

    tsting

--- amplify/backend/function/lambdaapi/index.py
+++ amplify/backend/function/lambdaapi/index.py
@@ -2,13 +2,20 @@ import requests
 import os
 import json
 
+from datetime import datetime, timedelta
+
 def lambda_handler(event, context):
     petfinder_api_url = 'https://api.petfinder.com/v2/animals'
     petfinder_api_key = os.environ.get('PET_FINDER_API_KEY')
     petfinder_api_secret = os.environ.get('PET_FINDER_API_SECRET')
 
     # Obtain access token
-    access_token = get_petfinder_access_token(petfinder_api_key, petfinder_api_secret)
+    access_token, expiration_time = get_petfinder_access_token(petfinder_api_key, petfinder_api_secret)
+
+    # Check if the token is expired
+    if datetime.utcnow() > expiration_time:
+        # Refresh the token
+        access_token, expiration_time = get_petfinder_access_token(petfinder_api_key, petfinder_api_secret)
 
     headers = {
         'Authorization': f'Bearer {access_token}',
@@ -18,7 +25,6 @@ def lambda_handler(event, context):
     response = requests.get(petfinder_api_url, headers=headers)
     print('Response Text:', response.text)
 
-
     if response.status_code == 200:
         return {
             'statusCode': 200,
@@ -27,7 +33,7 @@ def lambda_handler(event, context):
                 'Access-Control-Allow-Credentials': 'true',
                 'Content-Type': 'application/json',
             },
-            'body': json.dumps({ "animals": [] }),
+            'body': json.dumps({"animals": []}),
         }
     else:
         return {
@@ -39,6 +45,7 @@ def lambda_handler(event, context):
             },
             'body': json.dumps({'error': f'An error occurred while fetching data from Petfinder API: {response.text}'}),
         }
+
 
 def get_petfinder_access_token(api_key, api_secret):
     token_url = 'https://api.petfinder.com/v2/oauth2/token'
@@ -53,12 +60,17 @@ def get_petfinder_access_token(api_key, api_secret):
         token_response = requests.post(token_url, data=token_data)
         token_json = token_response.json()
         access_token = token_json.get('access_token')
+        expires_in = token_json.get('expires_in')
 
-        return access_token
+        # Calculate expiration time
+        expiration_time = datetime.utcnow() + timedelta(seconds=expires_in)
+
+        # Print or log expiration time
+        print('Token Expiration Time:', expiration_time)
+
+        return access_token, expiration_time
     except Exception as e:
         print('Error obtaining Petfinder access token:', str(e))
-
-
 
 
 # make sure you're in the virtual env
