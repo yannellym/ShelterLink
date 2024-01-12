ae730b7a2eef44728b039cab085046722076a964
Yannelly M <yannellym@Yannellys-Air.attlocal.net>
2024-01-12 17:20:18 -0500

    adding mail

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,47 +1,55 @@
 import React, { useState } from 'react';
-import '../styles/Emailform.css'
+import '../styles/Emailform.css';
+import emailjs from 'emailjs-com';
 
-
-
-const EmailForm = ({ petData, onSendEmail }) => {
+const EmailForm = ({ petData }) => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
     const [emailSent, setEmailSent] = useState(false);
-    const defaultMessage = 
-    `Hi, my name is ${storedUser.attributes.name}. I'm interested in adopting ${petData.name} and would love more information about their background, temperament, and the adoption process. Thank you!`;
+    const defaultMessage = `Hi, my name is ${storedUser.attributes.name}. I'm interested in adopting ${petData.name} and would love more information about their background, temperament, and the adoption process. Thank you!`;
 
     const [message, setMessage] = useState(defaultMessage);
-    const subject = `Inquiry for ${petData.name} ID: ${petData.id}`
+    const subject = `Inquiry for ${petData.name} ID: ${petData.id}`;
 
-    const handleSendEmail = () => {
-        console.log(storedUser.attributes.email, petData.contact.email, subject, message)
-        // Call the provided onSendEmail function with user's email and message
-        onSendEmail(storedUser.attributes.email, petData.contact.email, subject, message);
-    
-        // Update the state to indicate that the email has been sent
-        setEmailSent(true);
+    const sendEmail = async (toEmail, fromEmail, subject, message) => {
+        const templateParams = {
+            to_name: toEmail,  // Modify with the actual recipient's name
+            from_name: fromEmail,     // Modify with your name
+            message,
+        };
+
+        await emailjs.send('service_ogebq0b', 'template_nozw0nh', templateParams, 'Von3vL9e5hbQRbQnR');
+    };
+
+    const handleSendEmail = async () => {
+        try {
+            await sendEmail('ab.luis04@gmail.com', storedUser.attributes.email, subject, message);
+            setEmailSent(true);
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
     };
 
     return (
         <div className="email-form">
-        <h3>Interested in {petData.name}?</h3>
-        <div>
-            <label style={{ display: 'block' }}>Shelter's Email:</label>
-            <input type="email" value={petData.contact.email} />
-            <label>Your Email:</label>
-            <input type="email" value={storedUser.attributes.email}/>
-        </div>
-        <div>
-            <label>Message:</label>
-            <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
-        </div>
-        <div className="button-container">
-            <button className="inquiry_btn" onClick={handleSendEmail} disabled={emailSent}>
-                {emailSent ? 'Email Sent' : 'Send Email'}
-            </button>
-            <p className="bottom_text">Send additional inquiries to the shelter at: {petData.contact.email}</p>
-        </div>
+            <h3>Interested in {petData.name}?</h3>
+            <div>
+                <label style={{ display: 'block' }}>Shelter's Email:</label>
+                <input type="email" value={petData.contact.email} readOnly />
+                <label>Your Email:</label>
+                <input type="email" value={storedUser.attributes.email} readOnly />
+            </div>
+            <div>
+                <label>Message:</label>
+                <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
+            </div>
+            <div className="button-container">
+                <button className="inquiry_btn" onClick={handleSendEmail} disabled={emailSent}>
+                    {emailSent ? 'Email Sent' : 'Send Email'}
+                </button>
+                <p className="bottom_text">Send additional inquiries to the shelter at: {petData.contact.email}</p>
+            </div>
         </div>
     );
-    };
+};
 
 export default EmailForm;
