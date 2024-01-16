379b0357c884d47423b76a0f4a3308f609edc221
Yannelly M <yannellym@Yannellys-Air.attlocal.net>
2024-01-16 14:44:23 -0500

    working on forum

--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Forum.css';
 import bd4 from '../images/bd4.jpeg';
+import coming_soon from '../images/coming_soon.png';
 
 const Forum = () => {
   const [newSubject, setNewSubject] = useState('');
@@ -25,6 +26,12 @@ const Forum = () => {
 
   const [selectedTopic, setSelectedTopic] = useState(null);
   const [newPost, setNewPost] = useState('');
+  const [expandedPosts, setExpandedPosts] = useState([]);
+  const [charCount, setCharCount] = useState(0);
+
+  const handleReadMore = (index) => {
+    setExpandedPosts((prevExpanded) => [...prevExpanded, index]);
+  };
 
   const handleTopicSelection = (topic) => {
     setSelectedTopic(topic);
@@ -50,7 +57,10 @@ const Forum = () => {
     setTopics((prevTopics) =>
       prevTopics.map((topic) =>
         topic.title === selectedTopic.title
-          ? { ...topic, posts: [...topic.posts, newPostData] }
+          ? {
+              ...topic,
+              posts: [newPostData, ...topic.posts], // Prepend new post to existing posts
+            }
           : topic
       )
     );
@@ -65,6 +75,9 @@ const Forum = () => {
     setSelectedTopic(null);
   }, []);
 
+  useEffect(() => {
+    setCharCount(newPost.length);
+  }, [newPost]);
 
   // FUNCTION to fetch a random image of the type of animal and the breed in case the pet doesn't
   // have a photo. In case this fails, use our coming_soon photo.
@@ -76,14 +89,14 @@ const Forum = () => {
         return response.url;
       }
       // Return the placeholder image if the request fails
-      return 'coming_soon;'
+      return coming_soon;
     } catch (error) {
       console.error('Error fetching placeholder image:', error);
       // Return the placeholder image in case of an error
-      return 'coming_soon';
+      return {coming_soon};
     }
   };
-  
+
   return (
     <div className="forum-container">
       <div className="topics-list">
@@ -98,83 +111,96 @@ const Forum = () => {
       </div>
       <div className="thread-container">
         {selectedTopic ? (
-          <div>
-            <h3>Current topic: {selectedTopic.title}</h3>
-            {selectedTopic.posts && selectedTopic.posts.length > 0 ? (
               <div>
-                {selectedTopic.posts.map((post, index) => (
-                  <div
-                    key={post.id}
-                    className={`post ${index % 2 === 0 ? 'even' : 'odd'}`}
-                  >
-                    <h4>{post.subject}</h4>
-                    <p>{post.content}</p>
-                    <p>Posted by: {post.user.username} | Date: {new Date(post.id).toLocaleString()}</p>
-                  </div>
-                ))}
+              <h3>Current topic: {selectedTopic.title}</h3>
+              {selectedTopic.posts && 
+                <p>{selectedTopic.welcomeMessage}</p>
+              }
+              <div className="write-post-div">
+                <h4>Create a post:</h4>
+                <div className="post-input-container">
+                  <input
+                    type="text"
+                    value={newSubject}
+                    onChange={(e) => setNewSubject(e.target.value)}
+                    placeholder="Subject"
+                    className="post-input"
+                  />
+                  <textarea
+                    value={newPost}
+                    onChange={(e) => setNewPost(e.target.value)}
+                    placeholder="Write your post..."
+                    className="post-textarea"
+                  />
+                </div>
+                <div className="post-bottom-container">
+                  <p>Character Count: {charCount}</p>
+                  <button onClick={handlePostSubmit} className="post-button">
+                    Post
+                  </button>
+                </div>
               </div>
-            ) : (
-              <p>{selectedTopic.welcomeMessage}</p>
-            )}
-            <div className="write-post-div">
-              <input
-                type="text"
-                value={newSubject}
-                onChange={(e) => setNewSubject(e.target.value)}
-                placeholder="Subject"
-              />
-              <textarea
-                value={newPost}
-                onChange={(e) => setNewPost(e.target.value)}
-                placeholder="Write your post..."
-              />
-              <button onClick={handlePostSubmit}>Post</button>
-            </div>
-
             <h3>Current posts:</h3>
             {topics.map((topic) => (
-            <div key={topic.id}>
+              <div key={topic.id}>
                 {topic.title === selectedTopic.title &&
-                topic.posts && topic.posts.map((post, index) => (
+                  topic.posts && topic.posts.map((post, index) => (
                     <div className="post-container" key={post.id}>
-                    {index % 2 === 0 ? ( // If index is even, align image to the left
+                      {index % 2 === 0 ? ( // If index is even, align image to the left
                         <>
-                        <img src={post.image} alt={post.subject} className="left-image" />
-                        <div className={`post ${index % 2 === 0 ? 'even' : 'odd'}`}>
+                          <img src={post.image} alt={post.subject} className="left-image" />
+                          <div className={`post ${index % 2 === 0 ? 'even' : 'odd'}`}>
                             <div className="post-content">
-                            <h4>{post.subject.toUpperCase()}</h4>
-                            <p>{post.content}</p>
-                            <p>Posted by: {post.user.username} on {new Date(post.id).toLocaleDateString()} @ {new Date(post.id).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                              <h4>{post.subject.toUpperCase()}</h4>
+                              <p>
+                                {expandedPosts.includes(index) ? post.content : (
+                                  <>
+                                    {post.content.length > 100 ? `${post.content.substring(0, 100)}...` : post.content}
+                                    <span className="read-more" onClick={() => handleReadMore(index)}>
+                                      Read more
+                                    </span>
+                                  </>
+                                )}
+                              </p>
+                              <p>Posted by: {post.user.username} on {new Date(post.id).toLocaleDateString()} @ {new Date(post.id).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
                             </div>
-                        </div>
+                          </div>
                         </>
-                    ) : ( // If index is odd, align image to the right
+                      ) : ( // If index is odd, align image to the right
                         <>
-                        <div className={`post ${index % 2 === 0 ? 'even' : 'odd'}`}>
+                          <div className={`post ${index % 2 === 0 ? 'even' : 'odd'}`}>
                             <div className="post-content">
-                            <h4>{post.subject.toUpperCase()}</h4>
-                            <p>{post.content}</p>
-                            <p>Posted by: {post.user.username} on {new Date(post.id).toLocaleDateString()} @ {new Date(post.id).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+                              <h4>{post.subject.toUpperCase()}</h4>
+                              <p>
+                                {expandedPosts.includes(index) ? post.content : (
+                                  <>
+                                    {post.content.length > 200 ? `${post.content.substring(0, 200)}...` : post.content}
+                                    <span className="read-more" onClick={() => handleReadMore(index)}>
+                                      Read more
+                                    </span>
+                                  </>
+                                )}
+                              </p>
+                              <p>Posted by: {post.user.username} on {new Date(post.id).toLocaleDateString()} @ {new Date(post.id).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
                             </div>
-                        </div>
-                        <img src={post.image} alt={post.subject} className="right-image" />
+                          </div>
+                          <img src={post.image} alt={post.subject} className="right-image" />
                         </>
-                    )}
+                      )}
                     </div>
-                ))}
-            </div>
+                  ))}
+              </div>
             ))}
-          </div>
-        ) : (
-          <div className="welcome-div">
-            <h1> Welcome to our community forums! <br/> Please select a topic to join the discussion. </h1>
-            <img src={bd4} alt="bulldog" className="centered-image" />
-          </div>
-        )}
+            </div>
+          ) : (
+            <div className="welcome-div">
+              <h1> Welcome to our community forums! <br/> Please select a topic to join the discussion. </h1>
+              <img src={bd4} alt="bulldog" className="centered-image" />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default Forum;
-
+    );
+  }
+  
+  export default Forum;
